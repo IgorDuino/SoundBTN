80b44ec76dc5c2295ce084f0380f4c34dba8b088
IgorDuino <74809945+IgorDuino@users.noreply.github.com>
2021-02-17 14:48:52 +0300

    Црф

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SoundBTN
-Play any funny sounds by clicking on a button or phrase while talking to friends or streaming
+
+### Play any funny sounds by clicking on a button or phrase while talking to friends or streaming
+# Installation
+1. `Download `
+
+
+Полезная статья - https://vk.com/@tproger-python-gui-pyqt
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets
+import design  # Это наш конвертированный файл дизайна
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
