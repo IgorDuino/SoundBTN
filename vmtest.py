8803569cb8883d80f6bc7d492f5a7fe8282adb26
IgorDuino <74809945+IgorDuino@users.noreply.github.com>
2021-02-18 17:33:41 +0300

    Alpha

--- vmtest.py
+++
@@ -1,6 +0,0 @@
-from pygame._sdl2 import get_num_audio_devices, get_audio_device_name
-from pygame import mixer
-
-mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)')
-mixer.music.load("sounds/29-bruh.mp3")
-mixer.music.play()
